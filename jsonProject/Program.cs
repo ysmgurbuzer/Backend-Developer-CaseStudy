b2f98e241665a241df1781948ae0df87c492ef94
Yeşim Gürbüzer <ysmgurbuzerr@gmail.com>
2023-06-18 18:34:41 +0300

    case study
    Json parser and packing code

---
+++ jsonProject/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Vertex
+{
+    public int x { get; set; }
+    public int y { get; set; }
+}
+
+public class ShapeBoundary
+{
+    public List<Vertex> vertices { get; set; }
+}
+
+public class DescriptionData
+{
+    public string description { get; set; }
+    public ShapeBoundary boundingPoly { get; set; }
+
+    public static IEnumerable<DescriptionData> FromJsonInput(DescriptionData input)
+    {
+        // We created each row as a separate DescriptionData object
+        var lines = input.description.Split('\n');
+        foreach (var line in lines)
+        {
+            yield return new DescriptionData
+            {
+                description = line,
+                boundingPoly = input.boundingPoly,
+            };
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = @".\response.json";
+        string json = File.ReadAllText(filePath);
+
+        // We converted each entry in the JSON file into DescriptionData object and added it to the list
+        var receiptData = new List<DescriptionData>();
+        var jsonInputs = JsonConvert.DeserializeObject<List<DescriptionData>>(json);
+        foreach (var jsonInput in jsonInputs)
+        {
+            var descriptionData = new DescriptionData
+            {
+                description = jsonInput.description,
+                boundingPoly = jsonInput.boundingPoly
+            };
+            receiptData.Add(descriptionData);
+        }
+
+        int lineNumber = 1; 
+        int? lastY = null; // Last y coordinate
+        int threshold = 10; //We set a threshold for a new line
+        Dictionary<int, string> lines = new Dictionary<int, string>(); // We used a dictionary to store the lines
+
+        foreach (var descData in receiptData)
+        {
+            // We check if this is a new line
+            var currentY = descData.boundingPoly.vertices[0].y;
+            if (lastY.HasValue && Math.Abs(currentY - lastY.Value) > threshold)
+            {
+                lineNumber++;
+            }
+            lastY = currentY;
+
+            if (lineNumber == 1)
+            {
+                lines[lineNumber] = descData.description;
+            }
+            else
+            {
+                if (lines.ContainsKey(lineNumber))
+                {
+                    lines[lineNumber] += " " + descData.description;
+                }
+                else
+                {
+                    lines[lineNumber] = descData.description;
+                }
+            }
+        }
+
+        foreach (var line in lines)
+        {
+            Console.WriteLine($"Line: {line.Key}, Text: {line.Value}");
+        }
+    }
+}
---
+++ packagingCode/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var codeManager = new CodeManager();
+
+     
+        var codes = codeManager.GenerateCodes(1000);
+
+      
+        if (codeManager.CheckCode(codes[0]))
+        {
+            Console.WriteLine("CODE CORRECT!"); 
+        }
+        else
+        {
+            Console.WriteLine("CODE WRONG!"); 
+        }
+    }
+
+    public class CodeManager
+    {
+        private static readonly char[] AvailableCharacters =
+               { 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'K', 'L', 'M', 'N', 'P', 'R', 'T', 'X', 'Y', 'Z', '2', '3', '4', '5', '7', '9' };
+        private static int counter = 0;
+
+        public List<string> GenerateCodes(int count)
+        {
+            return Enumerable.Range(1, count)
+                             .Select(_ => GenerateCode())
+                             .ToList();
+        }
+
+        private string GenerateCode()
+        {
+            counter = (counter + 1) % (int)Math.Pow(AvailableCharacters.Length, 8);
+            var temp = counter;
+
+
+            // // The counter loops up to 8 times the length of the available characters, and we generate a different code each loop
+            return new string(Enumerable.Range(0, 8)
+                                        .Select(i => AvailableCharacters[temp / (int)Math.Pow(AvailableCharacters.Length, i) % AvailableCharacters.Length])
+                                        .Reverse()
+                                        .ToArray());
+        }
+
+        public bool CheckCode(string code)
+        {
+            // We check the validity of the code
+            return code.Length == 8 && code.All(c => AvailableCharacters.Contains(c));
+        }
+    }
+}
